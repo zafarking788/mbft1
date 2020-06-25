8b7c92cb2f7c64c76c18a08ac92bdd9a66e3e540
zafarking788 <56456546+zafarking788@users.noreply.github.com>
2020-06-25 19:58:07 +0500

    Update crack.py

--- app/crack.py
+++ app/crack.py
@@ -114,6 +114,17 @@ class Brute:
 						obj[2]+'123', obj[2]+'12345',
 						obj[3]+'123', obj[3]+'12345',
 					]
+
+					elif len(obj) == 5:
+					listpass = [
+						'sayang', 'Pakistan',
+						'pakistan', '786786'
+						'sayang123', 'Pakistan',
+						'pakistan', '786786'
+						 obj[0]+'786', obj[1]+'786'
+						'Sayang', '123456789'
+						'123456'
+					]
 				else:
 					listpass = [
 						'sayang', 'Pakistan',
