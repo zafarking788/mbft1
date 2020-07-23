7bcc39a1cdd10fde6b26bf9c551724b6b2b4b038
zafarking788 <56456546+zafarking788@users.noreply.github.com>
2020-07-23 15:43:50 +0500

    Update run.py

--- run.py
+++ run.py
@@ -14,7 +14,7 @@
 import os, sys, shutil
 from app import main as app
 
-base_url = 'https://mbasic.facebook.com'
+base_url = 'https://www.facebook.com'
 
 if sys.version_info.major != 2:
 	sys.exit('\n\033[0;91m[WARNING] Please use python 2 version\033[0m')
